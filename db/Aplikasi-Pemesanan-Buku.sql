7df8d2f1d3344a7a133c8458bb4d13ccfad37369
Lukman Hakim <opaslowdance@engineer.com>
2012-01-04 11:16:22 +0700

    Modify Database

--- db/Aplikasi-Pemesanan-Buku.sql
+++ db/Aplikasi-Pemesanan-Buku.sql
@@ -12,3 +12,14 @@ Harga VARCHAR(50) NOT NULL,
 Deskripsi TEXT,
 Sampul VARCHAR(50)
 )ENGINE=INNODB;
+
+DROP TABLE IF EXISTS Account;
+CREATE TABLE Account
+(
+id INT(5) NOT NULL PRIMARY KEY auto_increment,
+username VARCHAR(50) NOT NULL,
+password VARCHAR(50) NOT NULL,
+email VARCHAR(50) NOT NULL,
+sex ENUM("male","female") NOT NULL,
+adress text NOT NULL
+)ENGINE=INNODB;
